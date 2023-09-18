92b976e91033b7afedd73d5fa387ff3d7121f0bc
SchrodingersText <130055324+SchrodingersText@users.noreply.github.com>
2023-09-18 14:46:21 +0200

    Add color-change effect to grid on mouseover

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const gridContainer = document.querySelector(".grid-container");
 
-window.onload = function() {
-   for (let i = 1; i <= 256; i++) {
-      let gridSquare = document.createElement("div");
-      gridSquare.classList.add("grid-square");
-      gridContainer.appendChild(gridSquare);
-   };
+function generateGrid() {
+    for (let i = 1; i <= 256; i++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        gridContainer.appendChild(gridSquare);
+     };
 };
+
+generateGrid();
+
+const grid = document.querySelectorAll(".grid-square")
+
+grid.forEach(div => {
+    div.addEventListener("mouseover", () => {
+        div.classList.add("hover");
+    });
+});
